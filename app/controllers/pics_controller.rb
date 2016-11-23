db0b5c838dfcc32aeee3ab38e2313d9c58cc0382
Sundars0 <Sundars04@outlook.com>
2016-11-23 11:03:25 +0000

    CRUD functionality

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,49 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
-  def index    
+  def index
+    @pics = Pic.all.order("created_at DESC")
   end
+
+  def show
+  end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+    if @pic.save
+      redirect_to @pic, notice: "Yess! was posted"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update    
+    if @pic.update(pic_params)
+      redirect_to pic_path, notice: "Pic Successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
+  end
+
+  private
+
+    def pic_params
+      params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+      @pic = Pic.find(params[:id])
+    end
 end
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,2 +1,6 @@
 class Pic < ApplicationRecord
+  scope :sorted, -> {order("created_at DESC")}
+
+  validates :title, presence: true, length: {minimum: 5, maximum: 100}
+  validates :description, presence:true, length: {minimum: 10}
 end
